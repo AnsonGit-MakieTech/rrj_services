cd1cc8780aa40fd01cdd2f7c19bdb5d84c37d958
AnsonGit-MakieTech <techmakie@gmail.com>
2026-05-27 11:16:59 +0800

    feat: create inital model

--- base/models.py
+++ base/models.py
@@ -45,24 +45,75 @@ class Service(models.Model):
 
     
 class BookingRequest(models.Model):
+    # System Information
     owner = models.ForeignKey(AuthenticatedUser, on_delete=models.CASCADE , related_name='booking_requests')
     progress = models.CharField(
         max_length=255, choices=( 
-            
+            ('pending_quotation' , 'Pending Quotation'),
+            ('quotation_sent' , 'Quotation Sent'),
+            ('waiting_for_payment' , 'Waiting for Payment'),
+            ('payment_verification' , 'Payment Verification'),
+            ('booking_confirmed' , 'Booking Confirmed'),
+            ('scheduled' , 'Scheduled'),
+            ('in_progress' , 'In Progress'),
+            ('completed' , 'Completed'),
         ), 
-        default='p'
+        default='pending_quotation'
     )
-    """
-        Pending Quotation
-        Quotation Sent
-        Waiting for Payment
-        Payment Verification
-        Booking Confirmed
-        Scheduled
-        In Progress
-        Completed
-    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Customer Information
+    full_name = models.CharField(max_length=255 , blank=True, null=True)
+    email = models.CharField(max_length=255 , blank=True, null=True)
+    contact_number = models.CharField(max_length=255 , blank=True, null=True)
+    full_address = models.TextField( blank=True, null=True)
+
+    # Service Information
+    service = models.ForeignKey(Service, on_delete=models.CASCADE , related_name='booking_requests')
+    urgency_level = models.CharField(
+        max_length=255, choices=( 
+            ('low' , 'Low'),
+            ('medium' , 'Medium'),
+            ('high' , 'High'),
+            ('emergency' , 'Emergency'),
+        ), 
+        default='low'
+    )
+    preferred_date = models.DateField( blank=True, null=True)
+    square_meters = models.FloatField( default=0.0) # Square Meters (SQM)
+    project_location = models.TextField( blank=True, null=True)
+    service_description = models.TextField( blank=True, null=True)
+    problem_description = models.TextField( blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.pk} - {self.full_name}"
+
+
+class BookingAttachment(models.Model):
+    booking_request = models.ForeignKey(BookingRequest, on_delete=models.CASCADE , related_name='attachments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Attachment Information
+    file = models.FileField(upload_to='attachments/', blank=True, null=True)
+
+
+    def __str__(self):
+        return f"{self.pk} - {self.file.name}"
 
 
 
 
+class ChatMessage(models.Model):
+    booking_request = models.ForeignKey(BookingRequest, on_delete=models.CASCADE , related_name='chat_messages')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Chat Message Information
+    message = models.TextField( blank=True, null=True)
+    sender = models.ForeignKey(AuthenticatedUser, on_delete=models.CASCADE , related_name='chat_messages')
+    receiver = models.ForeignKey(AuthenticatedUser, on_delete=models.CASCADE , related_name='chat_messages')
+
+    def __str__(self):
+        return f"{self.pk} - {self.message}"
